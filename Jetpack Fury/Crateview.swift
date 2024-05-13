17acaaa6010cf021343c9b2ce3d06124e2bf69a3
chris robles <croblesquezad@bsd220.org>
2024-05-13 09:31:31 -0500

    jetpack moving

---
+++ Jetpack Fury/Crateview.swift
@@ -0,0 +1,18 @@
+//
+//  Crateview.swift
+//  Jetpack Fury
+//
+//  Created by christopher robles quezada on 5/10/24.
+//
+
+import SwiftUI
+
+struct Crateview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Crateview()
+}
